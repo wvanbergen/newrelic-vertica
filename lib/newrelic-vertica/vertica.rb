5105e5b96e8203fd44084170282378f7d2ce6e3e
Willem van Bergen <willem@vanbergen.org>
2013-03-24 23:20:09 -0400

    Second attempt.

--- lib/newrelic-vertica/vertica.rb
+++ lib/newrelic-vertica/vertica.rb
@@ -1,14 +1,46 @@
 require 'new_relic/agent/method_tracer'
+require 'benchmark'
 
-DependencyDetection.defer do
-  depends_on do
-    defined?(::Vertica)
-  end
+module NewRelic
+  module Agent
+    module Instrumentation
+      module VerticaInstrumentation
 
-  executes do
-    ::Vertica::Connection.class_eval do
-      add_method_tracer :query, 'Database/Vertica/query'
-      add_method_tracer :copy, 'Database/Vertica/copy'
+        def self.included(klass)
+          klass.class_eval do
+            alias_method :query_without_instrumentation, :query
+            alias_method :query, :query_with_instrumentation
+          end
+        end
+
+        def query_with_instrumentation(sql, options = {}, &block)
+          result = nil
+          duration = Benchmark.realtime do
+            result = query_without_instrumentation(sql, options, &block)
+          end
+
+          if NewRelic::Agent.is_execution_traced?
+            NewRelic::Agent.instance.transaction_sampler.notice_sql(sql, nil, duration)
+          end
+
+          return result
+        end
+      end
     end
   end
 end
+
+DependencyDetection.defer do
+  depends_on do
+    p 'checking dependencies'
+    defined?(::Vertica) && defined?(::Vertica::Connection)
+  end
+
+  executes do
+    p 'installing newrelic extention'
+    ::Vertica::Connection.instance_eval do
+      include ::NewRelic::Agent::Instrumentation::VerticaInstrumentation
+    end
+  end
+end
+
